6b370295bc571131820b6701a2415a3ee96e00c5
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-02-20 13:22:04 +0800

    Update author-sol.cpp

--- author-sol.cpp
+++ author-sol.cpp
@@ -15,7 +15,7 @@ int main() {
 		} else {
 		 	// first expression = a itself (nearly good number)
 		 	// second expression = product of a and b (good number)
-		 	// third expression = product of a and (b + 1) (nearly good number, because it's not divisible by product of a and b anymore)
+		 	// third expression = product of a and (b + 1) (nearly good number, because it's not divisible by the product of a and b anymore)
 		 	// increasing multiples of a
 		 	cout << "YES" << '\n';
 			cout << a << " " << a * 1LL * b << " " << a * 1LL * (b + 1) << '\n';
